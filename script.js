fb488131cfea4b6e141aeb7ce3e7d32552a97c33
Teakosprey <said200318@outlook.com>
2026-07-06 19:31:38 -0600

    Cambio grande en todo el proyecto

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// modo oscuro/claro
-const themeBtn = document.getElementById("toggle-theme");
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  themeBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
-});
-
-// cargar proyectos desde el json
+// carar proyectos desde el json
 async function loadProjects() {
   const res = await fetch("projects.json");
   const projects = await res.json();
@@ -15,9 +8,9 @@ async function loadProjects() {
     const card = document.createElement("div");
     card.className = "project-card";
     card.innerHTML = `
-      <h3>${p.name}</h3>
-      <p>${p.description}</p>
-      <a href="${p.link}" target="_blank">Ver proyecto</a>
+      <h3>${p.name}</h3><br>
+      <p>${p.description}</p><br>
+      <a href="${p.link}" target="_blank" style="text-decoration: none; color: #279af1; font-weight: bold;">Ver proyecto</a>
     `;
     container.appendChild(card);
   });
